66abc41382bb2913cf53a152fc4cd8444a79905c
shinstealer <39997731+Shinstealer@users.noreply.github.com>
2019-12-19 15:06:45 +0900

    work all categories in menu

--- dev/src/shinstealer/address/model/PersonListWrapper.java
+++ dev/src/shinstealer/address/model/PersonListWrapper.java
@@ -5,18 +5,23 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ * 연락처 리스트를 감싸는 헬퍼 클래스이다.
+ * XML로 저장하는 데 사용된다.
+ *
+ * @author Marco Jakob
+ */
 @XmlRootElement(name = "persons")
 public class PersonListWrapper {
 
-	private List<Person> person;
+    private List<Person> persons;
 
-	@XmlElement(name = "person")
-	public List<Person> getPerson() {
-		return person;
-	}
+    @XmlElement(name = "person")
+    public List<Person> getPersons() {
+        return persons;
+    }
 
-	public void setPerson(List<Person> person) {
-		this.person = person;
-	}
-
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
 }
